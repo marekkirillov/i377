e6da71e2846cd76512052cbebb1caf9e699cc43b
marekkirillov <marek.kirillov@itcollege.ee>
2013-09-25 15:35:10 +0300

    added ivy and updated build.xml

--- src/ITK/Hello.java
+++ src/ITK/Hello.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package ITK;
 
 import java.io.IOException;
@@ -15,29 +14,4 @@ public class Hello extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.getWriter().println("Hello!");
 	}
-
-
-
 }
-=======
-package ITK;
-
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-
-public class Hello extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
- 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().println("Hello");
-	}
-
-
-
-}
->>>>>>> 0711e4dac5f066340a21e3c2a807e7bc9596abd9
